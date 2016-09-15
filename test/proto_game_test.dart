f1c1762cc10b8ee700686cf394b2bf77928e46d5
Antonin <a.lebrard@gmail.com>
2016-09-15 16:35:01 +0200

    starting to make some unit tests

--- test/proto_game_test.dart
+++ test/proto_game_test.dart
@@ -8,7 +8,8 @@ import 'package:proto_game/proto_game.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Conditions Parsings', () {
+
+  group("decoding", (){
 
     Game game;
     Room start;
@@ -24,8 +25,32 @@ void main() {
       test2 = game.player.plateau.rooms.firstWhere((Room elem) => elem.name == "Test2");
     });
 
-    test('qsffqsf', () {
-      expect(true, isTrue);
+    test('decoding rooms', () {
+      expect(start != null, isTrue);
+      expect(start.id == "start".hashCode, isTrue);
+      expect(start.description == "the starting room", isTrue);
+      expect(start.name == "Start", isTrue);
+      expect(start.objects.length == 1, isTrue);
+      expect(start.nextRooms.length == 2, isTrue);
+      expect(start.nextRooms.containsKey(Direction.NORTH), isTrue);
+      expect(start.nextRooms.containsKey(Direction.EAST), isTrue);
+      expect(start.nextRooms[Direction.NORTH] == test1, isTrue);
+    });
+    test('decoding globals', (){
+      expect(game.globals.length == 3, isTrue);
+      expect(game.globals.firstWhere((GlobalVariable g) => g.name == "numGl").getType() == num, isTrue);
+      expect(game.globals.firstWhere((GlobalVariable g) => g.name == "numGl").getValue() == 0, isTrue);
+      expect(game.globals.firstWhere((GlobalVariable g) => g.name == "stringGl").getType() == String, isTrue);
+      expect(game.globals.firstWhere((GlobalVariable g) => g.name == "stringGl").getValue() == "test", isTrue);
+      expect(game.globals.firstWhere((GlobalVariable g) => g.name == "boolGl").getType() == bool, isTrue);
+      expect(game.globals.firstWhere((GlobalVariable g) => g.name == "boolGl").getValue() == true, isTrue);
+    });
+    test('decoding player', (){
+      expect(game.player.name == "Sicile", isTrue);
+      expect(game.player.inventory.length == 1, isTrue);
+      expect(game.player.inventory.first.name == "Bag", isTrue);
+      expect(game.player.properties.length == 2, isTrue);
+      expect(game.player.properties.keys.contains("firstPropertyPlayer"), isTrue);
     });
   });
 }
